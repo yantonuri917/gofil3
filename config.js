cfc0ebb97460e0331b1a733dd9d1eff2844ca6c7
Gofile <yantonuri917@gmail.com>
2026-06-05 21:56:40 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+window.API_CONFIG = {
+    URL: 'https://tsdawkagnwtldchuygtv.supabase.co/rest/v1',
+    KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzZGF3a2Fnbnd0bGRjaHV5Z3R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwODM3NTEsImV4cCI6MjA5NTY1OTc1MX0.a8koDAgUJdDQ5PL19ixFb8Ty6WHOfWpdtbs4ajU1mIA' // Gunakan Anon Public Key
+};
