e741c8586928c374335faf2abfac8b1dc6787310
Gofile <yantonuri917@gmail.com>
2026-06-12 18:34:53 +0700

    Refactor Histats tracking script initialization

--- tracker.js
+++ tracker.js
@@ -1,20 +1,33 @@
- 
-    history.replaceState(null, "", "/");
-    document.title = "gofil3.com";
+var _Hasync=_Hasync||[];
 
-    if (!sessionStorage.getItem("histats_hit")) {
-      sessionStorage.setItem("histats_hit", "1");
 
-      var _Hasync = _Hasync || [];
-      _Hasync.push(['Histats.start','1,4971849,4,0,0,0,00000000']);
-      _Hasync.push(['Histats.fasi','1']);
-      _Hasync.push(['Histats.track_hits','']);
 
-      (function() {
-        var hs = document.createElement('script');
-        hs.async = true;
-        hs.src = '//s10.histats.com/js15_as.js';
-        document.body.appendChild(hs);
-      })();
-    }
-    
+_Hasync.push(['Histats.start','1,4971849,4,0,0,0,00000000']);
+
+
+
+_Hasync.push(['Histats.fasi','1']);
+
+
+
+_Hasync.push(['Histats.track_hits','']);
+
+
+
+(function(){
+
+
+
+  var hs=document.createElement('script');hs.type='text/javascript';hs.async=true;
+
+
+
+  hs.src=('//s10.histats.com/js15_as.js');
+
+
+
+  (document.head||document.body).appendChild(hs);
+
+
+
+})();
